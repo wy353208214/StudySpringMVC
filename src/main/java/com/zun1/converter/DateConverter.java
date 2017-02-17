0acdc28d22b58b3e423f90d77fa3599ac92812a6
王洋 <wangyangbayern@gmail.com>
2017-02-17 17:07:55 +0800

    添加convert

---
+++ src/main/java/com/zun1/converter/DateConverter.java
@@ -0,0 +1,30 @@
+package com.zun1.converter;
+
+import org.springframework.core.convert.converter.Converter;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by 王洋 on 2017/2/17.
+ */
+public class DateConverter implements Converter<String, Date>{
+
+    private String pattern;
+
+    public DateConverter(String pattern) {
+        this.pattern = pattern;
+    }
+
+    public Date convert(String s) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat();
+        simpleDateFormat.applyLocalizedPattern(pattern);
+        simpleDateFormat.setLenient(false);
+        try {
+            return simpleDateFormat.parse(s);
+        } catch (ParseException e) {
+            throw new IllegalArgumentException(s);
+        }
+    }
+}
